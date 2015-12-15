f0d8891351be1bfe1502f244fdebedecba1dd0ac
Ashish Gupta <ashishg251@gmail.com>
2015-12-15 10:20:16 +0530

    Pushing D3 Assignment iles

---
+++ main.js
@@ -0,0 +1,303 @@
+var country_continent =
+{
+  Australia : " AUSTRALIA ",
+  Nauru : " AUSTRALIA ",
+  "Solomon Islands" : " AUSTRALIA ",
+  Fiji : " AUSTRALIA ",
+  "New Zealand" : " AUSTRALIA ",
+  Tonga : " AUSTRALIA ",
+  Kiribati : " AUSTRALIA ",
+  Palau : " AUSTRALIA ",
+  Tuvalu : " AUSTRALIA ",
+  "Marshall Islands" : " AUSTRALIA ",
+  "Papua New Guinea" : " AUSTRALIA ",
+  Vanuatu : " AUSTRALIA ",
+  Micronesia : " AUSTRALIA ",
+  Samoa : " AUSTRALIA ",
+  Algeria : " AFRICA ",
+  Angola : " AFRICA ",
+  Benin : " AFRICA ",
+  Botswana : " AFRICA ",
+  "Burkina Faso" : " AFRICA ",
+  Burundi : " AFRICA ",
+  Cameroon : " AFRICA ",
+  "Cape Verde" : " AFRICA ",
+  "Central African Republic" : " AFRICA ",
+  Chad : " AFRICA ",
+  Comoros : " AFRICA ",
+  "Congo - Brazzaville" : " AFRICA ",
+  "Congo - Kinshasa" : " AFRICA ",
+  "Côte d’Ivoire" : " AFRICA ",
+  Djibouti : " AFRICA ",
+  Egypt : " AFRICA ",
+  "Equatorial Guinea" : " AFRICA ",
+  Eritrea : " AFRICA ",
+  Ethiopia : " AFRICA ",
+  Gabon : " AFRICA ",
+  Gambia : " AFRICA ",
+  Ghana : " AFRICA ",
+  Guinea : " AFRICA ",
+  "Guinea-Bissau" : " AFRICA ",
+  Kenya : " AFRICA ",
+  Lesotho : " AFRICA ",
+  Liberia : " AFRICA ",
+  Libya : " AFRICA ",
+  Madagascar : " AFRICA ",
+  Malawi : " AFRICA ",
+  Mali : " AFRICA ",
+  Mauritania : " AFRICA ",
+  Mauritius : " AFRICA ",
+  Mayotte : " AFRICA ",
+  Morocco : " AFRICA ",
+  Mozambique : " AFRICA ",
+  Namibia : " AFRICA ",
+  Niger : " AFRICA ",
+  Nigeria : " AFRICA ",
+  Rwanda : " AFRICA ",
+  Réunion : " AFRICA ",
+  "Saint Helena" : " AFRICA ",
+  Senegal : " AFRICA ",
+  Seychelles : " AFRICA ",
+  "Sierra Leone" : " AFRICA ",
+  Somalia : " AFRICA ",
+  "South Africa" : " AFRICA ",
+  Sudan : " AFRICA ",
+  Swaziland : " AFRICA ",
+  "São Tomé and Príncipe" : " AFRICA ",
+  Tanzania : " AFRICA ",
+  Togo : " AFRICA ",
+  Tunisia : " AFRICA ",
+  Uganda : " AFRICA ",
+  "Western Sahara" : " AFRICA ",
+  Zambia : " AFRICA ",
+  Zimbabwe : " AFRICA ",
+  Anguilla : " AMERICA ",
+  "Antigua and Barbuda" : " AMERICA ",
+  Argentina : " SOUTH AMERICA ",
+  Aruba : " AMERICA ",
+  Bahamas : " AMERICA ",
+  Barbados : " AMERICA ",
+  Belize : " AMERICA ",
+  Bermuda : " AMERICA ",
+  Bolivia : " SOUTH AMERICA ",
+  Brazil : " SOUTH AMERICA ",
+  "British Virgin Islands" : " AMERICA ",
+  Canada : " AMERICA ",
+  "Cayman Islands" : " AMERICA ",
+  Chile : " SOUTH AMERICA ",
+  Colombia : " SOUTH AMERICA ",
+  "Costa Rica" : " AMERICA ",
+  Cuba : " AMERICA ",
+  Dominica : " AMERICA ",
+  "Dominican Republic" : " AMERICA ",
+  Ecuador : " SOUTH AMERICA ",
+  "El Salvador" : " AMERICA ",
+  "Falkland Islands" : " AMERICA ",
+  "French Guiana" : " AMERICA ",
+  Greenland : " AMERICA ",
+  Grenada : " AMERICA ",
+  Guadeloupe : " AMERICA ",
+  Guatemala : " AMERICA ",
+  Guyana : " SOUTH AMERICA ",
+  Haiti : " AMERICA ",
+  Honduras : " AMERICA ",
+  Jamaica : " AMERICA ",
+  Martinique : " AMERICA ",
+  Mexico : " AMERICA ",
+  Montserrat : " AMERICA ",
+  "Netherlands Antilles" : " AMERICA ",
+  Nicaragua : " AMERICA ",
+  Panama : " AMERICA ",
+  Paraguay : " SOUTH AMERICA ",
+  Peru : " SOUTH AMERICA ",
+  "Puerto Rico" : " AMERICA ",
+  "Saint Barthélemy" : " AMERICA ",
+  "Saint Kitts and Nevis" : " AMERICA ",
+  "Saint Lucia" : " AMERICA ",
+  "Saint Martin" : " AMERICA ",
+  "Saint Pierre and Miquelon" : " AMERICA ",
+  "Saint Vincent and the Grenadines" : " AMERICA ",
+  Suriname : " SOUTH AMERICA ",
+  "Trinidad and Tobago" : " AMERICA ",
+  "Turks and Caicos Islands" : " AMERICA ",
+  "U.S. Virgin Islands" : " AMERICA ",
+  "United States" : " AMERICA ",
+  Uruguay : " SOUTH AMERICA ",
+  Venezuela : " SOUTH AMERICA ",
+  Afghanistan : " ASIA ",
+  Armenia : " ASIA ",
+  Azerbaijan : " ASIA ",
+  Bahrain : " ASIA ",
+  Bangladesh : " ASIA ",
+  Bhutan : " ASIA ",
+  Brunei : " ASIA ",
+  Cambodia : " ASIA ",
+  China : " ASIA ",
+  Cyprus : " ASIA ",
+  Georgia : " ASIA ",
+  "Hong Kong SAR China" : " ASIA ",
+  India : " ASIA ",
+  Indonesia : " ASIA ",
+  Iran : " ASIA ",
+  Iraq : " ASIA ",
+  Israel : " ASIA ",
+  Japan : " ASIA ",
+  Jordan : " ASIA ",
+  Kazakhstan : " ASIA ",
+  Kuwait : " ASIA ",
+  Kyrgyzstan : " ASIA ",
+  Laos : " ASIA ",
+  Lebanon : " ASIA ",
+  "Macau SAR China" : " ASIA ",
+  Malaysia : " ASIA ",
+  Maldives : " ASIA ",
+  Mongolia : " ASIA ",
+  "Myanmar [Burma]" : " ASIA ",
+  Nepal : " ASIA ",
+  "Neutral Zone" : " ASIA ",
+  "North Korea" : " ASIA ",
+  Oman : " ASIA ",
+  Pakistan : " ASIA ",
+  "Palestinian Territories" : " ASIA ",
+  Philippines : " ASIA ",
+  Qatar : " ASIA ",
+  "Saudi Arabia" : " ASIA ",
+  Singapore : " ASIA ",
+  "South Korea" : " ASIA ",
+  "Sri Lanka" : " ASIA ",
+  Syria : " ASIA ",
+  Taiwan : " ASIA ",
+  Tajikistan : " ASIA ",
+  Thailand : " ASIA ",
+  "Timor-Leste" : " ASIA ",
+  Turkey : " ASIA ",
+  Turkmenistan : " ASIA ",
+  "United Arab Emirates" : " ASIA ",
+  Uzbekistan : " ASIA ",
+  Vietnam : " ASIA ",
+  Yemen : " ASIA ",
+  Albania : " EUROPE ",
+  Andorra : " EUROPE ",
+  Austria : " EUROPE ",
+  Belarus : " EUROPE ",
+  Belgium : " EUROPE ",
+  "Bosnia and Herzegovina" : " EUROPE ",
+  Bulgaria : " EUROPE ",
+  Croatia : " EUROPE ",
+  Cyprus : " EUROPE ",
+  "Czech Republic" : " EUROPE ",
+  Denmark : " EUROPE ",
+  "East Germany" : " EUROPE ",
+  Estonia : " EUROPE ",
+  "Faroe Islands" : " EUROPE ",
+  Finland : " EUROPE ",
+  France : " EUROPE ",
+  Germany : " EUROPE ",
+  Gibraltar : " EUROPE ",
+  Greece : " EUROPE ",
+  Guernsey : " EUROPE ",
+  Hungary : " EUROPE ",
+  Iceland : " EUROPE ",
+  Ireland : " EUROPE ",
+  "Isle of Man" : " EUROPE ",
+  Italy : " EUROPE ",
+  Jersey : " EUROPE ",
+  Latvia : " EUROPE ",
+  Liechtenstein : " EUROPE ",
+  Lithuania : " EUROPE ",
+  Luxembourg : " EUROPE ",
+  Macedonia : " EUROPE ",
+  Malta : " EUROPE ",
+  "Metropolitan France" : " EUROPE ",
+  Moldova : " EUROPE ",
+  Monaco : " EUROPE ",
+  Montenegro : " EUROPE ",
+  Netherlands : " EUROPE ",
+  Norway : " EUROPE ",
+  Poland : " EUROPE ",
+  Portugal : " EUROPE ",
+  Romania : " EUROPE ",
+  Russia : " EUROPE ",
+  "San Marino" : " EUROPE ",
+  Serbia : " EUROPE ",
+  "Serbia and Montenegro" : " EUROPE ",
+  Slovakia : " EUROPE ",
+  Slovenia : " EUROPE ",
+  Spain : " EUROPE ",
+  "Svalbard and Jan Mayen" : " EUROPE ",
+  Sweden : " EUROPE ",
+  Switzerland : " EUROPE ",
+  Ukraine : " EUROPE ",
+  "Union of Soviet Socialist Republics" : " EUROPE ",
+  "United Kingdom" : " EUROPE ",
+  "Vatican City" : " EUROPE ",
+  "Åland Islands" : " EUROPE "
+}
+
+var fs = require("fs");
+
+fs.readFile("WDI_Data.csv","UTF-8", function (err,data) {
+  if (err)
+  {
+    return err;
+  }
+  else
+  {
+    var g_data=data.toString();
+    var lines=g_data.split("\n");
+    var filteredLines=[];
+    var result1 = [];
+
+    var CONTINENT={};
+    CONTINENT["ASIA"]={};
+    CONTINENT["AFRICA"]={};
+    CONTINENT["EUROPE"]={};
+    CONTINENT["AMERICA"]={};
+    CONTINENT["AUSTRALIA"]={};
+
+    var headers=lines[0].split(",");
+    headers[59] = headers[59].replace(/\n|\r/g, "");
+    for(var i=0;i<lines.length;i++)
+    {
+      var currentline=lines[i].split(",");
+      if( currentline[2]==="GDP per capita (constant 2005 US$)")
+      {
+        filteredLines.push(lines[i]);
+      }
+    }
+
+    var main=[];
+    for(var i=0;i<filteredLines.length;i++)
+    {
+      var currentLine=filteredLines[i].split(",");
+      if(country_continent[currentLine[0]]!=undefined)
+      {
+        var continentName=country_continent[currentLine[0]].trim();
+        for(var j=4;j<59;j++)
+        {
+          if(main[j-4]===undefined)
+          {
+            main[j-4]={};
+            main[j-4]["year"]=headers[j];
+          }
+
+          if(main[j-4][continentName]===undefined)
+            main[j-4][continentName]=0;
+
+          if(currentLine[j]!="")
+          {
+            main[j-4][continentName]+=parseFloat(currentLine[j]);
+          }
+
+        }
+      }
+    }
+
+    var json=JSON.stringify(main,null,4);
+    fs.writeFile("GDP_Problem3.json",json,function(err){
+      if(err) {
+      console.log(err);
+      }
+    });
+  }
+});
